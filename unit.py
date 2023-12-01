4a1094a98604b58c2e88fd74b2602c112084d3bb
IvaBoo <151389363+BooFirstNew@users.noreply.github.com>
2023-12-01 04:16:44 +0100

    remove

---
+++ unit.py
@@ -0,0 +1,40 @@
+image: /assets/social-media-preview.png
+twitter:
+---
+
+
+
+Recently, there have been vile online attacks 
+looking to remove him from the FSF board of directors 
+this happen before in an organized fashion with other 
+prominent free software activists and programmers. 
+We will not stand idly this time, when an icon 
+of this community is attacked.
+
+FSF is an autonomous body that is capable of treating 
+its members in a fair, unbiased fashion, and should not 
+FSF to consider the arguments against RMS objectively 
+and to truly understand the meaning of his words and actions.
+
+Historically, RMS has been expressing his views in ways 
+that upset many people. He is usually more focused on the 
+philosophical underpinnings, and pursuing the objective 
+truth and linguistic purism, while underemphasising people's 
+which we feel is happening in the open letter calling for his removal. 
+His words need to be interpreted in this context and 
+taking into account that more often than not, 
+he is not looking to put things diplomatically.
+
+he is being persecuted over are not relevant to 
+his ability to lead a community such as the FSF. 
+as much as anyone else. Members and supporters do not 
+his right to freedom of thought and speech.
+
+**To the FSF:**
+Removing RMS will hurt FSF's image and will deal 
+You have no part in choosing the leadership of any communities. 
+Especially not via another mob attack which does not remotely 
+by better people such as Richard Stallman.
+
+To sign, please [submit a pull
+- Or add your signature here: [https://codeberg.org/rms-support-letter/rms-support-letter/issues/1](https://codeberg.org/rms-support-letter/rms-support-letter/issues/1)
